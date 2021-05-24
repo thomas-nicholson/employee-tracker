4f10cf7e482f040ffcca1b81354657f7e87c0f17
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-05-24 18:43:22 +1000

    Fixed mysql database connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host : 'localhost',
-  port : 3606,
+  port : 3306,
   user : 'root',
   password : 'MySpace_64',
   database : 'employee_db'
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+connection.query('SELECT * FROM employee_db.employee', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
 });
